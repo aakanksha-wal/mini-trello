64c5e934ac19589e63fa4a8235b6927a7738382d
akanksha-wal <aakanksha.k@westagilelabs.com>
2020-11-15 15:05:02 +0530

    Updates

---
+++ precache-manifest.d6acaf00783e6ae1979ed6e400d6eb82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397652c54050a27ebe2cd0d714b24406",
+    "url": "/mini-trello/index.html"
+  },
+  {
+    "revision": "f25ecaa1234f3f182d56",
+    "url": "/mini-trello/static/css/main.2e025893.chunk.css"
+  },
+  {
+    "revision": "a22b4a89a7c3de561098",
+    "url": "/mini-trello/static/js/2.78acce4b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mini-trello/static/js/2.78acce4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25ecaa1234f3f182d56",
+    "url": "/mini-trello/static/js/main.de8e46f4.chunk.js"
+  },
+  {
+    "revision": "caf7592017cd94d4302b",
+    "url": "/mini-trello/static/js/runtime-main.e77635a0.js"
+  },
+  {
+    "revision": "f3efed1410a0b335f143090c13011c8c",
+    "url": "/mini-trello/static/media/attachment.f3efed14.jpg"
+  }
+]);
